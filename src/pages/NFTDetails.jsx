9f9302e5c3e943b22bc2c888528c6776addeef4b
William Cole <ioetbc@gmail.com>
2022-01-03 13:55:17 +0000

    styles for the product page

--- src/pages/NFTDetails.jsx
+++ src/pages/NFTDetails.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react'
 import * as qs from 'query-string'
+import dayjs from 'dayjs'
+import { GlassMagnifier } from 'react-image-magnifiers'
 import styles from '../styles/NFTDetails.module.css'
 
 import { NFTContent } from '../content/NFTContent'
+import { PrimaryButton } from '../components/PrimaryButton'
 
 export const NFTDetails = ({ location }) => {
   const { category, title } = qs.parse(location.search)
@@ -14,24 +17,38 @@ export const NFTDetails = ({ location }) => {
     body.style.overflow = 'scroll'
   }, [])
 
+  console.log(dayjs().add(5, 'days').format('dddd D MMM'))
+
   return (
     <>
-      <div className="thing">
-        <div className={styles.aboutContactWrapper}>
-          <img className={styles.image} src={data?.image} />
-          <div className={styles.transcript}>
-            <h1 className={styles.title}>{data?.title}</h1>
-            <p>
-              On mobile the images on the left will just live in the sliding page.Seventh NFT lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Porro praesentium neque esse. Seventh NFT lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Porro praesentium neque esse. Seventh NFT lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Porro praesentium neque esse. Seventh NFT lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Porro praesentium neque esse. Seventh NFT lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Porro praesentium neque esse. Seventh NFT lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Porro praesentium neque esse. Seventh NFT lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Porro praesentium neque esse. Seventh NFT lorem ipsum dolor sit amet
-            </p>
-          </div>
+      <div className={styles.details}>
+        <h1 className={`${styles.title} ${styles.mobile}`}>{data?.title}</h1>
+        <GlassMagnifier
+          className={styles.image}
+          imageSrc={data?.image}
+          imageAlt={data?.alt}
+          square={true}
+          magnifierSize={200}
+          magnifierBorderSize={0}
+        />
+        <div className={styles.transcript}>
+          <h1 className={`${styles.title} ${styles.desktop}`}>{data?.title}</h1>
+          <p>
+            On mobile the images on the left will just live in the sliding page.Seventh NFT lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Porro praesentium neque esse. Seventh NFT lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Porro praesentium neque esse. Seventh NFT lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Porro praesentium neque esse. Seventh NFT lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Porro praesentium neque esse. Seventh NFT lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Porro praesentium neque esse. Seventh NFT lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Porro praesentium neque esse. Seventh NFT lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Porro praesentium neque esse. Seventh NFT lorem ipsum dolor sit amet
+          </p>
+          <ul className={styles.detailsList}>
+            <li>DIMENSIONS: W/20cm H/30cm</li>
+            <li>PRICE: £45.00</li>
+            <li>Delivered by {dayjs().add(5, 'days').format('dddd D MMMM')}</li>
+          </ul>
+          <PrimaryButton text="PURCHASE" url="https://www.test.bymiles.co.uk/" />
         </div>
       </div>
     </>
